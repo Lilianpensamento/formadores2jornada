448b1f404ecd33e90a6ca566c6a6a99f9c375853
Lilianpensamento <140979604+Lilianpensamento@users.noreply.github.com>
2025-07-29 16:41:07 -0300

    awfqef

--- script.js
+++ script.js
@@ -14,11 +14,3 @@ const lista = document.querySelector ("#lista");
 lista.textContent = linguagens [0];
 lista.textContent = linguagens [1];
 lista.textContent = linguagens [2];
-
-let aluno1 = {
-  nome: "Gabriel",
-  idade: 13,
-  anoLetivo: "2ºsérie",
-  materiasFavoritas: ["Geografia","Programação","Arte"]
-
-
